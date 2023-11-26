3dab0debcf4885f0c0729b20bb29bf98eede9aeb
gurpreet8033 <gurpreetgh52@gmail.com>
2023-11-26 08:34:51 -0800

    added home page and menu page

--- src/app/MyCompenents/home/home.component.ts
+++ src/app/MyCompenents/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  isVisible: boolean = false;
+  handleClick=() => {
+    this.isVisible =!this.isVisible;
+    console.log("gk");
+    
+  }
 }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+}) 
+export class NavbarComponent {
+   menuList=[ 
+    {name:'Home'},
+    {name:'Menu'},
+    {name:'About'}
+   ]
+}
